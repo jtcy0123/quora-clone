cc873276b66161aeebccc6530a25eab6e1cb221e
jtcy0123 <jasmin0123@hotmail.com>
2017-06-26 00:22:49 +0800

    answer feature

---
+++ app/controllers/answer.rb
@@ -0,0 +1,63 @@
+# get form for user to post answer
+get '/questions/:id/answers/new' do
+  if logged_in?
+    @question = Question.find_by_id(params[:id])
+    erb :"static/answers"
+  else
+    flash[:info] = "You need to log in to answer the question."
+    redirect '/questions'
+  end
+end
+
+# create answer
+post '/questions/:id/answers' do
+  params[:answer][:user_id] = current_user.id
+  answer = Answer.new(params[:answer])
+  if answer.save
+    flash[:msg] = "Answer posted successfully"
+    redirect '/users/'+ current_user.id.to_s + '/answers'
+  else
+    flash[:error] = answer.errors.full_messages.join('. ')
+    redirect '/questions/'+ params[:answer][:question_id].to_s + '/answers'
+    redirect '/questions'
+  end
+end
+
+# show all answers user posted
+get '/users/:id/answers' do
+  @answers = Answer.where(user_id: params[:id]).order('created_at desc')
+  erb :"static/my_answers"
+end
+
+# delete particular answer
+delete '/answers/:id' do
+  answer = Answer.find(params[:id])
+  answer.delete
+  flash[:msg] = "Answer deleted"
+  redirect '/users/'+ current_user.id.to_s + '/answers'
+end
+
+# show particular question with all the respective answers
+get '/questions/:id/answers' do
+  @q = Question.find(params[:id])
+  @answers = Answer.where(question_id: params[:id])
+  erb :"static/q&a"
+end
+
+# get form for editing an answer
+get '/answers/:id/edit' do
+  @answer = Answer.find(params[:id])
+  @question = Question.find(@answer.question_id)
+  erb :'static/answers/edit'
+end
+
+# update edited answer
+put '/answers/:id' do
+  @answer = Answer.find(params[:id])
+  if @answer.update(params[:answer])
+    flash[:msg] = "Answer updated successfully"
+  else
+    flash[:error] = "Invalid input"
+  end
+  redirect '/users/' + current_user.id.to_s + '/answers'
+end
