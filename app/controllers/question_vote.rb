18853b16dce8e5e532d8736df360757c738cac97
jtcy0123 <jasmin0123@hotmail.com>
2017-06-27 02:32:32 +0800

    ajax for vote

--- app/controllers/question_vote.rb
+++ app/controllers/question_vote.rb
@@ -1,14 +1,16 @@
 # add vote to question
 post '/questions/:id/add' do
   if logged_in?
-    @new_vote = QuestionVote.new(user_id: current_user.id, question_id: params[:id])
-    if @new_vote.save
-      @new_vote.update_attributes(vote: 1)
+    @v = QuestionVote.new(user_id: current_user.id, question_id: params[:id])
+    if @v.save
+      @v.update_attributes(vote: 1)
     else
       @v = QuestionVote.find_by(user_id: current_user.id, question_id: params[:id])
       @v.vote == 0 ? @v.vote += 1 : @v.vote -=1
       @v.save
     end
+    a = {id: params[:id], vote: @v.vote}
+    return a.to_json
     redirect back
   else
     flash[:info] = "You need to log in to vote for the question."
